e3fca249447f398a927c97006dd44e07eb1a5f17
Abu-rafay <aburafayf@gmail.com>
2026-03-22 17:03:13 -0400

    Delete Project_2 directory

--- Project_2/Phase_1/Node.cpp
+++
@@ -1,5 +0,0 @@
-#include "Node.h"
- 
- Node::Node(): data(0), next(nullptr) {}  
-                  
- Node::Node(const int &d): data(d), next(nullptr) {}
--- Project_2/Phase_2/Node.cpp
+++
@@ -1,5 +0,0 @@
-#include "Node.h"
-
-Node::Node(): data(), next(nullptr) {}  
-                 
-Node::Node(const Employee &d): data(d), next(nullptr) {}
